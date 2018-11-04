8417dbf3a8418afef8113139a07f75ab3528ea80
Karl Stenerud <kstenerud@gmail.com>
2018-11-04 10:11:46 +0100

    Deduplicate code

--- configure-dev-user.sh
+++ configure-dev-user.sh
@@ -18,8 +18,15 @@ file_contains()
 
 get_homedir()
 {
-    user=$1
-    eval echo "~$user"
+    username=$1
+    eval echo "~$username"
+}
+
+chown_homedir()
+{
+    username=$1
+
+    chown $username:$(id -g $username) "$(get_homedir $username)"
 }
 
 add_to_file()
@@ -218,7 +225,7 @@ configure_quilt ${USERNAME}
 configure_dput ${USERNAME}
 configure_git ${USERNAME} "${GIT_USERNAME}" "${FULL_NAME}" "${EMAIL}" "${LP_NAME}"
 add_user_to_required_groups ${USERNAME}
-chown -R ${USERNAME}:$(id -g ${USERNAME}) "$(get_homedir $USERNAME)"
+chown_homedir ${USERNAME}
 
 
 echo 'Dev user configured successfully. Remember to set:
