2eb08437d8324c47328d59b16bf00e0b6114959a
Karl Stenerud <kstenerud@gmail.com>
2018-11-04 17:30:46 +0100

    Support re-attaching bind mounted dirs

--- map-vm-paths.sh
+++ map-vm-paths.sh
@@ -46,40 +46,49 @@ map_path()
 {
     old_path="$1"
     new_path="$2"
+    map_existing=$3
     old_path_bak="${old_path}.bak"
+    copy_old_path=true
 
     if is_bind_mounted "$old_path" "$new_path"; then
-        echo "Path [$old_path] is already bind mounted to [$new_path]. Skipping."
+        echo "Path [$old_path] is already bind mounted to [$new_path], skipping."
         return 0
     fi
 
     if [ ! -d "$old_path" ]; then
-        echo "Path [$old_path] doesn't exist. Skipping."
+        echo "Old path [$old_path] doesn't exist, skipping."
         return 0
     fi
 
     if [ -d "$new_path" ]; then
-        echo "Path [$new_path] already exists. Skipping."
-        return 0
+        if [ "$map_existing" == "true" ]; then
+            copy_old_path=false
+        else
+            echo "New path [$new_path] already exists, skipping. Use -e to map existing new path."
+            return 0
+        fi
     fi
 
-    if [ -d "$old_path_bak" ]; then
-        echo "Backup path [$old_path_bak] already exists. Skipping."
-        return 0
+    if [ "$copy_old_path" == "true" ]; then
+        if [ -d "$old_path_bak" ]; then
+            echo "Backup path [$old_path_bak] already exists, skipping."
+            return 0
+        fi
+
+        echo "Bind mounting [${old_path}] to [${new_path}], and creating backup [${old_path_bak}]."
+        copy_directory_contents "$old_path" "$new_path"
+        mv "${old_path}" "${old_path}.bak"
+        mkdir -p "${old_path}"
+    else
+        echo "Re-attaching [${old_path}] to [${new_path}], and creating backup [${old_path_bak}]."
     fi
 
-    echo "Bind mounting [${old_path}] to [${new_path}], and creating backup [${old_path_bak}]."
-    copy_directory_contents "$old_path" "$new_path"
-    mv "${old_path}" "${old_path}.bak"
-    mkdir -p "${old_path}"
     write_fstab_entry "$old_path" "$new_path"
 }
 
 show_help()
 {
     echo "Bind mounts the following paths to a virtual \"home dir\":
-
-Useful if you have a separate boot drive with limited space.
 
  * /var/lib/libvirt
  * /var/lib/lxd
@@ -88,7 +97,14 @@ Useful if you have a separate boot drive with limited space.
  * /var/snap/lxd/common
  * /var/snap/multipass/common
 
-Usage: $(basename $0) <virtual home dir>"
+Useful if you have a separate boot drive with limited space.
+
+Note: Old paths are backed up as (path).bak
+
+Usage: $(basename $0) [options] <virtual home dir>
+
+Options:
+    -e: If the new path already exists, map to it anyway (but don't copy)."
 }
 
 usage()
@@ -99,13 +115,16 @@ usage()
 
 #####################################################################
 
-COPY_CONTENTS=false
+MAP_EXISTING=false
 
-while getopts "?" o; do
+while getopts "?e" o; do
     case "$o" in
         \?)
             show_help
             exit 0
+            ;;
+        e)
+            MAP_EXISTING=true
             ;;
         *)
             usage
@@ -123,12 +142,12 @@ VIRT_HOME_DIR="$1"
 echo "Backing up /etc/fstab to /etc/fstab.bak"
  cp -a /etc/fstab /etc/fstab.bak
 
-map_path /var/lib/libvirt           "$VIRT_HOME_DIR/lib/libvirt"
-map_path /var/lib/lxd               "$VIRT_HOME_DIR/lib/lxd"
-map_path /var/lib/uvtool            "$VIRT_HOME_DIR/lib/uvtool"
-map_path /var/snap/docker/common    "$VIRT_HOME_DIR/snap/docker"
-map_path /var/snap/lxd/common       "$VIRT_HOME_DIR/snap/lxd"
-map_path /var/snap/multipass/common "$VIRT_HOME_DIR/snap/multipass"
+map_path /var/lib/libvirt           "$VIRT_HOME_DIR/lib/libvirt"    $MAP_EXISTING
+map_path /var/lib/lxd               "$VIRT_HOME_DIR/lib/lxd"        $MAP_EXISTING
+map_path /var/lib/uvtool            "$VIRT_HOME_DIR/lib/uvtool"     $MAP_EXISTING
+map_path /var/snap/docker/common    "$VIRT_HOME_DIR/snap/docker"    $MAP_EXISTING
+map_path /var/snap/lxd/common       "$VIRT_HOME_DIR/snap/lxd"       $MAP_EXISTING
+map_path /var/snap/multipass/common "$VIRT_HOME_DIR/snap/multipass" $MAP_EXISTING
 
 echo "Paths mapped. Re-mounting..."
 mount -a
