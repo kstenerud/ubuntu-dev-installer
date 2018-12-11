5c0f7e0738e573d341ca3f0a18beb4cbac68aa91
Karl Stenerud <karl.stenerud@canonical.com>
2018-12-11 16:13:22 +0100

    Add sbuild config

--- configure-dev-user.sh
+++ configure-dev-user.sh
@@ -3,15 +3,23 @@ set -eu
 
 show_help()
 {
-    echo "Congigures tooling and defaults for ubuntu development."
+    echo "Configures tooling and defaults for ubuntu development."
     echo
     echo "Usage: $(basename $0) [options] <username> <full name> <email> <git username> <lp username>"
-    echo "or:    $(basename $0) -g <username>"
+    echo "or:    $(basename $0) -r <username>"
     echo
     echo "Options:"
-    echo "  -G: Add user to needed groups and don't do anything else."
+    echo "  -r: Reinstall system-level config only (if you've reinstalled the OS and moved your homedir over)."
+    echo "  -h: Map the user's homedir to schroot"
+    echo "  -k: Run as the user: sbuild-update --keygen"
     echo "  -c: This is a console only user."
     echo "  -g: This is a GUI user."
+    echo
+    echo "Help Pages:"
+    echo " - https://wiki.ubuntu.com/SimpleSbuild"
+    echo " - https://wiki.debian.org/UsingQuilt"
+    echo " - http://manpages.ubuntu.com/manpages/cosmic/en/man1/dput.1.html"
+    echo " - https://git-scm.com/book/en/v2"
 }
 
 #####################################################################
@@ -35,7 +43,7 @@ chown_homedir()
 {
     username=$1
 
-    chown -R $username:$(id -g $username) "$(get_homedir $username)"
+    chown -R $username:$(id -g $username) "$(get_homedir $username)" || true
 }
 
 add_to_file()
@@ -147,10 +155,107 @@ configure_git()
     fi
 }
 
+add_schroot_mount()
+{
+    outside_path="$1"
+    inside_path="$2"
+    mount_line="$outside_path  $inside_path          none  rw,bind  0  0"
+
+    if ! grep "\\w$inside_path\\w" /etc/schroot/sbuild/fstab >>/dev/null; then
+        echo "$mount_line" >> /etc/schroot/sbuild/fstab
+    fi
+}
+
+configure_schroot()
+{
+    username=$1
+    distribution=$2
+    maintainer_name="$3"
+    maintainer_email="$4"
+    wants_keygen="$5"
+    wants_home_dir_mount="$6"
+
+    home_dir="$(get_homedir $username)"
+    mount_point="$home_dir/schroot"
+    sbuildrc="$home_dir/.sbuildrc"
+    mk_sbuildrc="$home_dir/.mk-sbuild.rc"
+
+    if [ $wants_keygen -ne 0 ]; then
+        su -c "sbuild-update --keygen" $username
+    fi
+
+    if [ $wants_home_dir_mount -ne 0 ]; then
+        add_schroot_mount "$home_dir" "$home_dir"
+    fi
+
+    if [ -d "$mount_point" ]; then
+        echo "schroot is already configured. To reconfigure, delete or move:"
+        echo " * $mount_point"
+        echo " * $sbuildrc"
+        echo " * $mk_sbuildrc"
+        return 0
+    fi
+
+    mkdir -p "$mount_point/build"
+    mkdir -p "$mount_point/logs"
+    mkdir -p "$mount_point/scratch"
+
+    add_schroot_mount "$mount_point/scratch" "/scratch"
+
+    if [ ! -f "$sbuildrc" ]; then
+        echo "# Name to use as override in .changes files for the Maintainer: field
+# (mandatory, no default!).
+\$maintainer_name='$maintainer_name <$maintainer_email>';
+
+# Default distribution to build.
+\$distribution = \"$distribution\";
+# Build arch-all by default.
+\$build_arch_all = 1;
+
+# When to purge the build directory afterwards; possible values are 'never',
+# 'successful', and 'always'.  'always' is the default. It can be helpful
+# to preserve failing builds for debugging purposes.  Switch these comments
+# if you want to preserve even successful builds, and then use
+# 'schroot -e --all-sessions' to clean them up manually.
+\$purge_build_directory = 'successful';
+\$purge_session = 'successful';
+\$purge_build_deps = 'successful';
+# \$purge_build_directory = 'never';
+# \$purge_session = 'never';
+# \$purge_build_deps = 'never';
+
+# Directory for writing build logs to
+\$log_dir=\"$mount_point/logs\";
+
+# don't remove this, Perl needs it:
+1;" >"$sbuildrc"
+    fi
+
+    if [ ! -f "$mk_sbuildrc" ]; then
+        echo "SCHROOT_CONF_SUFFIX=\"source-root-users=root,sbuild,admin
+source-root-groups=root,sbuild,admin
+preserve-environment=true\"
+# you will want to undo the below for stable releases, read \`man mk-sbuild\` for details
+# during the development cycle, these pockets are not used, but will contain important
+# updates after each release of Ubuntu
+SKIP_UPDATES=\"1\"
+SKIP_PROPOSED=\"1\"
+# if you have e.g. apt-cacher-ng around
+DEBOOTSTRAP_PROXY=http://127.0.0.1:3142/" >"$mk_sbuildrc"
+    fi
+}
+
 add_user_to_required_groups()
 {
     user=$1
-    add_user_to_groups $user adm sudo lxd kvm libvirt docker
+    add_user_to_groups $user \
+        adm \
+        sudo \
+        lxd \
+        kvm \
+        libvirt \
+        docker \
+        sbuild
 }
 
 usage()
@@ -161,10 +266,13 @@ usage()
 
 #####################################################################
 
-GROUPS_ONLY=false
+REINSTALL=0
 INSTALL_MODE=
+DISTRUBUTION=bionic
+WANTS_KEYGEN=0
+WANTS_HOME_DIR_MOUNT=0
 
-while getopts "gcG" o; do
+while getopts "gcrkhd:" o; do
     case "$o" in
         \?)
             show_help
@@ -176,8 +284,17 @@ while getopts "gcG" o; do
         g)
             INSTALL_MODE=gui
             ;;
-        G)
-            GROUPS_ONLY=true
+        r)
+            REINSTALL=1
+            ;;
+        k)
+            WANTS_KEYGEN=1
+            ;;
+        h)
+            WANTS_HOME_DIR_MOUNT=1
+            ;;
+        d)
+            DISTRUBUTION=$OPTARG
             ;;
         *)
             usage
@@ -186,13 +303,18 @@ while getopts "gcG" o; do
 done
 shift $((OPTIND-1))
 
-if [ "$GROUPS_ONLY" == "true" ]; then
+if [ "$EUID" -ne 0 ]; then
+    echo "$(basename $0) must run using sudo"
+    exit 1
+fi
+
+if [ "$REINSTALL" -ne 0 ]; then
     if (( $# != 1 )); then
         usage
     fi
     USERNAME=$1
 
-    echo "Adding user $USERNAME to groups only..."
+    echo "Reinstalling system level settings for $USERNAME..."
     add_user_to_required_groups
     exit 0
 fi
@@ -204,11 +326,6 @@ fi
 
 if (( $# != 5 )); then
     usage
-fi
-
-if [ "$EUID" -ne 0 ]; then
-    echo "$(basename $0) must run using sudo"
-    exit 1
 fi
 
 USERNAME=$1
@@ -227,6 +344,8 @@ modify_profile ${USERNAME} "${FULL_NAME}" "${EMAIL}" $INSTALL_MODE
 configure_quilt ${USERNAME}
 configure_dput ${USERNAME}
 configure_git ${USERNAME} "${GIT_USERNAME}" "${FULL_NAME}" "${EMAIL}" "${LP_NAME}"
+configure_schroot ${USERNAME} ${DISTRUBUTION} "${FULL_NAME}" "${EMAIL}" $WANTS_KEYGEN $WANTS_HOME_DIR_MOUNT
+
 add_user_to_required_groups ${USERNAME}
 chown_homedir ${USERNAME}
 
